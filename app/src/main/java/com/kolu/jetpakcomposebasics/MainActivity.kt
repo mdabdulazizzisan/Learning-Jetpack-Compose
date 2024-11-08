cbc22ccbfcd374cbdc3eb5519dcf573ac6755362
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-11-08 07:41:31 +0600

    Implemented both LazyColumn and LazyRow.

--- app/src/main/java/com/kolu/jetpakcomposebasics/MainActivity.kt
+++ app/src/main/java/com/kolu/jetpakcomposebasics/MainActivity.kt
@@ -4,24 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.kolu.jetpakcomposebasics.ui.theme.JetpakComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,12 +40,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    if(name.length > 5){
-        Icon(
-            imageVector = Icons.Default.Call,
-            contentDescription = null
+    Column {
+        LazyRow {
+            items(10) {
+                Icon(
+                    imageVector = Icons.Default.Call,
+                    contentDescription = null,
+                    Modifier.size(200.dp)
+                )
+            }
+        }
+
+        Box(
+            Modifier
+                .background(Color.LightGray)
+                .fillMaxWidth()
+                .height(10.dp)
         )
+        LazyColumn {
+            items(10) {
+                Icon(
+                    imageVector = Icons.Default.Call,
+                    contentDescription = null,
+                    Modifier.size(200.dp)
+                )
+            }
+        }
     }
+
 
 }
 
