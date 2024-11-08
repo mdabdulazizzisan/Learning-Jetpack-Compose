33bec4f8186277679fc15031725a62dde3ffc11f
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-11-08 07:27:13 +0600

    Learned about Image and Icon Composable.

--- app/src/main/java/com/kolu/jetpakcomposebasics/MainActivity.kt
+++ app/src/main/java/com/kolu/jetpakcomposebasics/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,30 +41,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .background(Color.Blue)
-            .padding(20.dp)
-            .size(400.dp)
-    ) {
-        Text(
-            text = "Hello $name!",
-            fontSize = 30.sp,
-            color = Color.White,
-            modifier = Modifier
-                .background(Color.Red)
-                .padding(10.dp)
-                .align(Alignment.BottomCenter)
-
-        )
-        Text(
-            text = "Second Text",
-            fontSize = 30.sp,
-            color = Color.LightGray
-        )
-    }
-
+    Image(painter = painterResource(
+        id = R.drawable.ic_launcher_background),
+        contentDescription = null)
+    Icon(
+        imageVector = Icons.Default.Call,
+        contentDescription = null
+    )
 }
 
 @Preview(showBackground = true)
