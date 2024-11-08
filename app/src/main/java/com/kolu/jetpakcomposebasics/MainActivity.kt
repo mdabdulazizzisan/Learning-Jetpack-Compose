bbeaf23bf66b80f62ffe6869ff9eef3edae5fd97
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-11-08 07:20:52 +0600

    Learned about the Box Composable.

--- app/src/main/java/com/kolu/jetpakcomposebasics/MainActivity.kt
+++ app/src/main/java/com/kolu/jetpakcomposebasics/MainActivity.kt
@@ -5,17 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,13 +35,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Row(
-        verticalAlignment = Alignment.Bottom,
-        horizontalArrangement = Arrangement.End,
+    Box(
+        contentAlignment = Alignment.Center,
         modifier = Modifier
             .background(Color.Blue)
             .padding(20.dp)
-            .fillMaxHeight()
+            .size(400.dp)
     ) {
         Text(
             text = "Hello $name!",
@@ -53,6 +49,8 @@ fun Greeting(name: String) {
             modifier = Modifier
                 .background(Color.Red)
                 .padding(10.dp)
+                .align(Alignment.BottomCenter)
+
         )
         Text(
             text = "Second Text",
