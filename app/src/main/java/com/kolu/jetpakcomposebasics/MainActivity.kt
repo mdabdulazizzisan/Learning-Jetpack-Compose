c3ceee59f81872b323a478377c857b32632ce899
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-11-11 01:21:26 +0600

    Implemented Simple counter app.

--- app/src/main/java/com/kolu/jetpakcomposebasics/MainActivity.kt
+++ app/src/main/java/com/kolu/jetpakcomposebasics/MainActivity.kt
@@ -4,23 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Call
-import androidx.compose.material3.Icon
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kolu.jetpakcomposebasics.ui.theme.JetpakComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,54 +24,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpakComposeBasicsTheme {
+                var count by remember {
+                    mutableIntStateOf(0)
+                }
 
-                    Greeting(
-                        name = "Android"
-                    )
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Column {
-        //Replacement of Horizontal recycler view
-        LazyRow {
-            items(10) {
-                Icon(
-                    imageVector = Icons.Default.Call,
-                    contentDescription = null,
-                    Modifier.size(200.dp)
-                )
-            }
-        }
-
-        Box(
-            Modifier
-                .background(Color.LightGray)
-                .fillMaxWidth()
-                .height(10.dp)
-        )
-        //Replacement of Vertical RecyclerView
-        LazyColumn {
-            items(10) {
-                Icon(
-                    imageVector = Icons.Default.Call,
-                    contentDescription = null,
-                    Modifier.size(200.dp)
-                )
+                Column(
+                    Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Text(text = count.toString(),
+                        fontSize = 30.sp)
+                    Button(onClick = {
+                        count++
+                    }) {
+                        Text(text = "Click Me!")
+                    }
+                }
             }
         }
     }
 
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpakComposeBasicsTheme {
-        Greeting("Android")
-    }
 }
