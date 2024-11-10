41d6cfd82eb582d522cdd713a31b5e9b3e719557
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-11-11 02:03:56 +0600

    Implemented the structure of the final app. Designing pending.

--- app/src/main/java/com/kolu/jetpakcomposebasics/MainActivity.kt
+++ app/src/main/java/com/kolu/jetpakcomposebasics/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.kolu.jetpakcomposebasics
 
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kolu.jetpakcomposebasics.ui.theme.JetpakComposeBasicsTheme
 
@@ -24,25 +40,51 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpakComposeBasicsTheme {
-                var count by remember {
-                    mutableIntStateOf(0)
+                var name by remember {
+                    mutableStateOf("")
+                }
+                var names by remember {
+                    mutableStateOf(listOf<String>())
                 }
 
                 Column(
-                    Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp, vertical = 50.dp)
+
                 ) {
-                    Text(text = count.toString(),
-                        fontSize = 30.sp)
-                    Button(onClick = {
-                        count++
-                    }) {
-                        Text(text = "Click Me!")
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = { text ->
+                                name = text
+                            },
+                            modifier = Modifier.weight(1f)
+                        )
+                        Spacer(modifier = Modifier.width(10.dp))
+                        OutlinedButton(
+                            shape = RoundedCornerShape(10),
+                            onClick = {
+                                if (name.isNotBlank()) {
+                                    names = names + name
+                                    name = ""
+                                }
+                            }
+                        ) {
+                            Text(text = "Add")
+                        }
+                    }
+                    LazyColumn {
+                        items(names){selectedName ->
+                            Text(
+                                text = selectedName
+                            )
+                        }
                     }
                 }
             }
         }
     }
-
 }
